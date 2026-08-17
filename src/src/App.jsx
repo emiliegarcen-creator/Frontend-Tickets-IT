2ce7a4eda99a8c1cc8b175bd7dd826974e9ad1c7
emiliegarcen-creator <emilie.garcen@anima.edu.uy>
2026-08-17 20:09:19 -0300

    Add files via upload

---
+++ src/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import Registro from "./Registro/Registro";
+import { InicioSesion } from "./Inicio de sesión/iniciodesesion";
+import { PaginaInicial } from "./Pagina inicial/paginainicial";
+
+function App() {
+    const [usuario, setUsuario] = useState("");
+
+    return (
+        <div>
+            {/* Esto es para que si usuario tiene un valor muestra la pagina inicial */}
+            {usuario.length > 0
+                ? <PaginaInicial usuario={usuario} />
+                : <InicioSesion setUsuario={setUsuario} />
+            }
+        </div>
+    );
+}
+
+export default App;
