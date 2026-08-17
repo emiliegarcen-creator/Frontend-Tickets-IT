4d1dfb33013147432595e26945e847c710ec9b75
emiliegarcen-creator <emilie.garcen@anima.edu.uy>
2026-08-17 20:14:06 -0300

    Add files via upload

---
+++ src/Pagina inicial/paginainicial.jsx
@@ -0,0 +1,8 @@
+export function PaginaInicial({ usuario }) {
+    return (
+        <div>
+            <h1>Bienvenido a la página inicial</h1>
+            <h2>{usuario}</h2>
+        </div>
+    );
+}
