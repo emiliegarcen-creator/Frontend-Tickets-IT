edcff735be873d74cd2b0b047f79407b7a7d4c18
emiliegarcen-creator <emilie.garcen@anima.edu.uy>
2026-08-17 20:12:09 -0300

    Delete src/src directory

--- src/src/Pagina inicial/paginainicial.jsx
+++
@@ -1,8 +0,0 @@
-export function PaginaInicial({ usuario }) {
-    return (
-        <div>
-            <h1>Bienvenido a la página inicial</h1>
-            <h2>{usuario}</h2>
-        </div>
-    );
-}
